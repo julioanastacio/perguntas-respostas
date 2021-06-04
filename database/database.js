fcec22df155fd3a3bc00338c5279b9967195c74a
Julio Anastacio Pereira Neto <julio.neto581@gmail.com>
2021-06-04 19:52:02 -0300

    App Done
    Término do projeto de perguntas e respostas .

---
+++ database/database.js
@@ -0,0 +1,7 @@
+const sequelize = require('sequelize');
+const connection = new sequelize('guiaperguntas', 'root', 'admin', {
+  host: 'localhost',
+  dialect: 'mysql',
+});
+
+module.exports = connection;
